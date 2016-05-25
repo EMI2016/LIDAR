beb8975d3ebbff36fd5df040a85eee945ee2ab26
EMI2016 <rojas.emily@pucp.pe>
2016-05-25 17:22:26 -0500

    Add files via upload

--- ho1.py
+++ ho1.py
@@ -64,7 +64,7 @@ def linea_l(x1,y1,x2,y2,disp,a1,b1):
             print 'Punto medio',a1,b1                  
         
     return [disp,a1,b1]
-
+"""
 def draw(img, corners, imgpts):
     corner = tuple(corners[0].ravel())
     cv2.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
@@ -79,7 +79,7 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((6*7,3), np.float32)
 objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 
-axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
+axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)"""
             
 
 uart_port = '/dev/ttyACM0'
@@ -107,7 +107,7 @@ if __name__ == '__main__':
         angles=np.array(angles)
         distances=np.array(distances)
 
-        ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+       """ ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
 
         if ret == True:
             cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
@@ -116,7 +116,7 @@ if __name__ == '__main__':
             rvecs, tvecs, inliers = cv2.solvePnPRansac(objp, corners, mtx, dist)
         # project 3D points to image plane
             imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
-            img = draw(img,corners,imgpts)
+            img = draw(img,corners,imgpts)"""
 	
         for i in range(len(distances)):
              if (angles[i]>-29.4 and angles[i]<29.4):
@@ -176,7 +176,7 @@ if __name__ == '__main__':
             print(str(e))
         
        
-        #cv2.imshow("img",img)
+        cv2.imshow("img",img)
         if k==27:
             break
     laser.laser_off()
